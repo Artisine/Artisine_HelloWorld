8e944ef92a1c6d214c5d1bed22734a0ca6687b5b
Artisine <Artisine@users.noreply.github.com>
2018-12-08 10:32:10 +0000

    Updated main.js

--- Artisine_HelloWorld/bin/Debug/AppX/js/main.js
+++ Artisine_HelloWorld/bin/Debug/AppX/js/main.js
@@ -1,7 +1,19 @@
 ﻿// Your code here!
 
+var Globals = {};
 window.onload = function () {
+    function g(e) { return document.querySelector(e); };
+    Globals["Canvas"] = g("#canvas");
+    Globals["Ctx"] = Globals["Canvas"].getContext("2d");
+    Globals["Entities"] = {};
+    update();
+};
 
+function update() {
+    Globals["Ctx"].clearRect(0, 0, Globals["Canvas"].width, Globals["Canvas"].height);
+    Globals["Ctx"].fillRect(Globals["Canvas"].width * 0.25, Globals["Canvas"].height * 0.25,
+        Globals["Canvas"].width * 0.75, Globals["Canvas"].height * 0.75);
+    setTimeout(update, 50);
 };
 
 //end all
--- Artisine_HelloWorld/js/main.js
+++ Artisine_HelloWorld/js/main.js
@@ -1,7 +1,48 @@
 ﻿// Your code here!
 
+var Globals = {};
 window.onload = function () {
+    function g(e) { return document.querySelector(e); };
+    Globals["Canvas"] = g("#canvas");
+    Globals["Canvas"].width = window.innerWidth;
+    Globals["Ctx"] = Globals["Canvas"].getContext("2d");
+    Globals["Entities"] = {};
+    Globals["Entities"].RectangleLoader = new Rectangle();
+    update();
+};
 
+function Rectangle() {
+    this.Color = "red";
+    this.Position = { X: 0, Y: Globals["Canvas"].height / 2 };
+    this.Size = { X: 20, Y: 10 };
+    this.Velocity = { X: 1, Y: 0 };
+    this.translate = function () {
+        this.Position.X += this.Velocity.X;
+        this.Position.Y += this.Velocity.Y;
+    };
+    this.render = function () {
+        var ctx = Globals["Ctx"];
+        var x = this.Position.X, y = this.Position.Y;
+        var sx = this.Size.X / 2, sy = this.Size.Y / 2;
+        ctx.beginPath();
+        ctx.moveTo(x - sx, y - sy);
+        ctx.lineTo(x - sx, y + sy);
+        ctx.lineTo(x + sx, y + sy);
+        ctx.lineTo(x + sx, y - sy);
+        ctx.lineTo(x - sx, y - sy);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fillStyle = this.Color;
+        ctx.fill();
+    };
+};
+
+function update() {
+    Globals["Ctx"].clearRect(0, 0, Globals["Canvas"].width, Globals["Canvas"].height);
+    for (var i in Globals["Entities"]) {
+        Globals["Entities"][i].render();
+    }
+    setTimeout(update, 50);
 };
 
 //end all
